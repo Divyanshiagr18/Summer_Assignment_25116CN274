4de45bcd5107fd0f666e8c5d0a7cc2fb3cd4fb24
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-08 20:12:13 +0530

    Added question 2 of day 8

---
+++ day_8/Question_30.java
@@ -0,0 +1,20 @@
+package day_8 ;
+import java.util.Scanner;
+public class Question_30
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner scnr=new Scanner (System.in);
+        System.out.println("enter no.of rows and column");
+        n=scnr.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
