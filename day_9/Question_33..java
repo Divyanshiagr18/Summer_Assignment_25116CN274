5084617bab3d5ff1cd4f51369bac65ed1780f8ad
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-09 16:52:40 +0530

    Added question 1 of day 9

---
+++ day_9/Question_33..java
@@ -0,0 +1,20 @@
+package day_9 ;
+import java.util.Scanner;
+public class Question_33
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner scnr=new Scanner (System.in);
+        System.out.println("enter no.of rows and column");
+        n=scnr.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            for (int j=i;j>=1;j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
