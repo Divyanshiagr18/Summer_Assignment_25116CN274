2bc7ac213919c1edcab992e3a0c666e3bfa4b248
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-17 19:43:58 +0530

    Added question 1 of day 16

---
+++ Day_16/Question_61.java
@@ -0,0 +1,31 @@
+package day_16 ;
+import java.util.*;
+public class Question_61 
+
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        System.out.println("Enter the number of Elements");
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        int n = num+1;
+        for(i = 0;i<num;i++)
+        {
+              arr[i]=sc.nextInt();
+        }
+        int len = arr.length;
+        int act_sum=n*(n+1)/2;
+        int sum = 0;
+        for(i=0;i<len;i++)
+        {
+            sum+=arr[i];
+        }
+        int diff = act_sum-sum;
+        System.out.println("missing element:"+diff);
+       
+
+    }
+    
+}
