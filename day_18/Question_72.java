aa8d3c2d925c6611535169c07c92edfe398321f2
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-18 18:50:41 +0530

    Added question 4 of day 18

---
+++ day_18/Question_72.java
@@ -0,0 +1,40 @@
+package day_18 ;
+import java.util.Scanner;
+
+public class Question_72
+ {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] < arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted in descending order: ");
+        for (int i = 0; i < n; i++)
+        {
+            System.out.println(arr[i] + " ");
+        }
+        
+      
+    }
+}   
