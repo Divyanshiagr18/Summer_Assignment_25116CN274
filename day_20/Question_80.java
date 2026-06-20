5e1c19cf5e981e4c93e2bf413f3526f9e89eae6e
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-20 19:00:50 +0530

    Added question 4 of day 20

---
+++ day_20/Question_80.java
@@ -0,0 +1,44 @@
+package day_20 ;
+import java.util.*;
+public class columnsum
+{
+    public static void main(String args[])
+    {
+        int sum=0;
+        Scanner scnr=new Scanner(System.in);
+        System.out.println("Enter size of row");
+        int r=scnr.nextInt();
+        System.out.println("enter size of column");
+        int c=scnr.nextInt();
+        int b=0;
+        System.out.println("Enter elements in array:");
+        int a[][]=new int[r][c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                a[i][j]=scnr.nextInt();
+            }
+        }
+        System.out.println("MATRIX:");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<r;i++)
+        {
+            while(b!=c)
+            {
+                sum+=a[b++][i];
+            }
+            b=0;
+            System.out.println("sum of column "+(i+1)+" -> "+sum);
+            sum=0;
+        }
+        
+    }
+}
