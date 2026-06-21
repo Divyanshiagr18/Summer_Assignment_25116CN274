1effa7cb375c4a10432be38af7c384ac5e3dd8f3
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-21 20:49:07 +0530

    Added question 4 of day 21

---
+++ day_21/Question_84.java
@@ -0,0 +1,15 @@
+package day_21 ;
+import java.util.*;
+public class Question_84
+{
+    public static void main(String args[])
+    {
+        String str="",strnew="";
+        int len;
+        Scanner scnr=new Scanner(System.in);
+        System.out.println("Enter a string");
+        str=scnr.nextLine();
+        strnew=str.toUpperCase();
+        System.out.println("STRING IN UPPER CASE: "+strnew);
+    }
+}
