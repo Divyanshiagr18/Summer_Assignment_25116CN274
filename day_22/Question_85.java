380992426598af94982dacca2b5945346273addc
Divyanshiagr18 <divyanshi.agrahari1811@gmail.com>
2026-06-22 20:53:36 +0530

    Added question 1 of day 22

---
+++ day_22/Question_85.java
@@ -0,0 +1,27 @@
+package day_21 ;
+import java.util.*;
+public class Question_85
+{
+    public static void main(String args[])
+    {
+        String str="",newstr="";
+        int len;
+        Scanner scnr=new Scanner(System.in);
+        System.out.println("Enter a string");
+        str=scnr.nextLine();
+        str=str.toUpperCase();
+        len=str.length();
+        for(int i=len-1;i>=0;i--)
+        {
+            newstr=newstr+str.charAt(i);
+        }
+        if(str.equalsIgnoreCase(newstr))
+        {
+            System.out.println(str+": ITS PALINDROME");
+        }
+        else
+        {
+            System.out.println(str+": ITS NOT PALINDROME");
+        }
+    }
+}
